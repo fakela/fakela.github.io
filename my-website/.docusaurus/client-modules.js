b7265f74bc5589dae6ccc062804d08a67d9dda06
Favour Kelvin <favourkelvin17@gmail.com>
2023-08-11 20:18:58 +0100

    update structure

--- my-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/workspace/fakela.github.io/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/workspace/fakela.github.io/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspace/fakela.github.io/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspace/fakela.github.io/my-website/src/css/custom.css'),
-];
