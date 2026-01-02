70f031a2c36dffc734da5632d1a3b19a314e3963
Favour Kelvin <favourkelvin17@gmail.com>
2026-01-02 05:04:46 +0000

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/favourkelvin/Documents/fakela.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/favourkelvin/Documents/fakela.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/favourkelvin/Documents/fakela.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/favourkelvin/Documents/fakela.github.io/src/css/custom.css'),
-];
